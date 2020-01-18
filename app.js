f0c3c02d43934eabcb5a60f18298e8f2477e602d
geoinfoma <59839570+geoinfoma@users.noreply.github.com>
2020-01-18 21:46:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ window.fn.open = function () {
     menu.open();
 };
 window.fn.load = function (page) {
-    var content = document.getElementById('content');
+	var content = document.getElementById('content');
     var menu = document.getElementById('menu');
     content.load(page)
             .then(menu.close.bind(menu));
 };
 
-document.addEventListener('init', function(event) {
-    if(event.target.id == "home") {
+document.addEventListener('init', function (event) {
+    if (event.target.id == "home") {
         openDb();
         getItems();
     }
@@ -28,14 +28,14 @@ function onSuccess(tx, r) {
 }
 
 function openDb() {
-    db = openDatabase("TripList", "1", "Trip list", 1024*1024);
-    db.transaction(function(tx) {
+    db = openDatabase("TripList", "1", "Trip list", 1024 * 1024);
+    db.transaction(function (tx) {
         tx.executeSql("CREATE TABLE IF NOT EXISTS items (ID INTEGER PRIMARY KEY ASC, item TEXT)", []);
     });
 }
 
 function getItems() {
-    db.transaction(function(tx) {
+    db.transaction(function (tx) {
         tx.executeSql("SELECT * FROM items", [], renderItems, onError);
     });
 }
@@ -47,7 +47,7 @@ function renderItems(tx, rs) {
     for (i = 0; i < rs.rows.length; i++) {
         var row = rs.rows.item(i);
         output += "<ons-list-item>" + row.item +
-                "<div class=\"right\"> <ons-button onclick='deleteItem(" + row.ID +");')><ons-icon icon=\"trash\"></ons-icon></ons-button> <ons-button onclick='showItem(" + row.ID +");')><ons-icon icon=\"globe\"></ons-icon></ons-button></div>" +
+                "<div class=\"right\"> <ons-button onclick='deleteItem(" + row.ID + ");')><ons-icon icon=\"trash\"></ons-icon></ons-button> <ons-button onclick='showItem(" + row.ID + ");')><ons-icon icon=\"globe\"></ons-icon></ons-button></div>" +
                 "</ons-list-item>";
     }
 
@@ -57,36 +57,32 @@ function renderItems(tx, rs) {
 function addItem() {
     var textbox = document.getElementById("item");
     var value = textbox.value;
-    
-    db.transaction(function(tx){
+
+    db.transaction(function (tx) {
         tx.executeSql("INSERT INTO items (item) VALUES (?)", [value], onSuccess, onError);
-        });
-    
-textbox.value = "";
-fn.load('home.html');
+    });
+
+    textbox.value = "";
+    fn.load('home.html');
 }
 
 function deleteItem(id) {
-    db.transaction(function(tx) {
+    db.transaction(function (tx) {
         tx.executeSql("DELETE FROM items WHERE ID=?", [id], onSuccess, onError)
     })
 }
 
-
 function showModal() {
-  var modal = document.querySelector('ons-modal');
-  modal.show();
-  setTimeout(function() {
-    modal.hide();
-  }, 1500);
+    var modal = document.querySelector('ons-modal');
+    modal.show();
+    setTimeout(function () {
+        modal.hide();
+    }, 1500);
 }
 
-
-
 function showItem(id) {
-	var text = document.getElementById("item");
+    var text = document.getElementById("item");
     text.show();
-   
 }
 
 
